be95dcf71d09d7b5fd211ba8f9f42602069d812f
Karen Mairena Diaz <kmairena@iesoft.net>
2022-10-03 10:58:44 -0600

    NUEVO

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // imports
 importScripts('js/sw-utils.js');//importar otro archivo para usar metodos
 
-const STATIC_CACHE    = 'static-v2';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
